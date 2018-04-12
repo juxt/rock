cc3a3b169b63b0b9e684dc13dfabce7b67861f56
James Conroy-Finn <git@invetica.co.uk>
2018-04-12 13:58:27 +0100

    Deploy an EC2 instance using Rock

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,11 +23,41 @@ data "template_file" "user_data" {
   template = "${file("${path.cwd}/rock.sh")}"
 }
 
+resource "aws_cloudwatch_log_group" "rock" {
+  name              = "rock"
+  retention_in_days = 7
+}
+
+resource "aws_iam_role" "rock" {
+  name = "rock_instance_profile"
+  path = "/"
+
+  assume_role_policy = "${file("assume-role-policy.json")}"
+}
+
+resource "aws_iam_instance_profile" "rock" {
+  name = "rock_instance_profile"
+  role = "${aws_iam_role.rock.name}"
+}
+
+resource "aws_iam_role_policy" "rock" {
+  name   = "rock_logs"
+  role   = "${aws_iam_role.rock.id}"
+  policy = "${file("policy.json")}"
+}
+
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all inbound traffic."
 
   ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -41,11 +71,12 @@ resource "aws_security_group" "allow_all" {
 }
 
 resource "aws_instance" "rock" {
-  ami               = "${data.aws_ami.rock.id}"
-  instance_type     = "t2.small"
-  availability_zone = "eu-west-1"
-  security_groups   = ["${aws_security_group.allow_all.id}"]
-  user_data         = "${data.template_file.user_data.rendered}"
+  ami                  = "${data.aws_ami.rock.id}"
+  instance_type        = "t2.small"
+  availability_zone    = "eu-west-1a"
+  iam_instance_profile = "${aws_iam_instance_profile.rock.name}"
+  security_groups      = ["${aws_security_group.allow_all.name}"]
+  user_data            = "${data.template_file.user_data.rendered}"
 
   # This needs to be created manually.
   key_name = "rock"
@@ -58,4 +89,8 @@ resource "aws_instance" "rock" {
     Name   = "rock"
     Source = "juxt/rock"
   }
+}
+
+output "address" {
+  value = "${aws_instance.rock.public_dns}"
 }
