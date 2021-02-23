e44350f8725b6762e128d38488c6caebe4009f27
Malcolm Sparks <mal@juxt.pro>
2021-02-23 23:40:37 +0000

    Install yay-bin, quicker to build AMI

--- scripts/install-yay.sh
+++ scripts/install-yay.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
 makepkg -si --noconfirm
 cd ..
-rm -rf yay
+rm -rf yay-bin
