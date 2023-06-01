d5da70f3f1c85607f9edd4fd8113b54ecce5a931
Chris Williams <cwi@juxt.pro>
2023-06-01 11:11:23 +0100

    Update reflector config for wider net of mirrors
    There are not enough quality mirrors in the GB region for 20 GB
    mirrors as recommended by the Arch Wiki

--- scripts/pacman-key.sh
+++ scripts/pacman-key.sh
@@ -4,4 +4,6 @@ set -eux
 
 pacman-key --init
 pacman-key --populate
-reflector --country "GB" --protocol https,http --score 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country GB,FR,DE --protocol https,http --age 72 --score 30 -n 20 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman --noconfirm -Sy archlinux-keyring
+sudo pacman --noconfirm -Syu
